dbfe247a5ad166fa9c1ece8874b8e5937d19adf3
ariadnana <ariadnaxm@gmail.com>
2016-10-24 17:20:06 +0200

    feina feta a classe

--- app/src/main/java/com/example/ariadna/calculadora_android/CalculatorActivity.java
+++ app/src/main/java/com/example/ariadna/calculadora_android/CalculatorActivity.java
@@ -2,8 +2,37 @@ package com.example.ariadna.calculadora_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
+    float num=0;
+    char op='n';
+    final TextView res = (TextView) findViewById(R.id.text);
+
+    public void display(View v){
+        Button b = (Button)v;
+        String text=res.getText().toString();
+        text=text+b.getText();
+        res.setText(text);
+    }
+
+    public void operacio(View s){
+        String text=res.getText().toString();
+        num = Float.parseFloat(text);
+        text="";
+        res.setText(text);
+    }
+
+    public void resol(){
+
+    }
+
+    public void clear(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
